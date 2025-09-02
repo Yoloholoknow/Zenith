0af1a6ea22f5fc49ab50454d0933feda8a4e9fa5
Yoloholoknow <charleshuang4130@gmail.com>
2025-09-02 11:06:31 -0400

    Initial Commit

---
+++ Zenith/ZenithApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZenithApp.swift
+//  Zenith
+//
+//  Created by Charles Huang on 9/2/25.
+//
+
+import SwiftUI
+
+@main
+struct ZenithApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
